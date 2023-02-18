8103110a272e70fe16131f5728c319afb24dd311
Damiano Cacchiarelli <damiano.cacchiarelli@studenti.unicam.it>
2023-02-18 10:58:31 +0100

    [Arduino uno - FIX] Fixed collision detection between ball and paddle, ball and board

--- src/game-loop.cpp
+++ src/game-loop.cpp
@@ -17,7 +17,7 @@ void GameLoop::update(GameEntity *gameEntities)
     Ball *ball = gameEntities->getBall();
     Paddle *paddle1 = gameEntities->getPaddle1();
     Paddle *paddle2 = gameEntities->getPaddle2();
-    ball->updateVelocity(0.5f, 1);
+    ball->updateVelocity(5, 2);
 
     paddle1->updateVelocity(0, 0.5f);
     paddle2->updateVelocity(0, -0.2f);
@@ -25,40 +25,7 @@ void GameLoop::update(GameEntity *gameEntities)
     int ups = 0;
     unsigned long lastTime = millis();
 
-    /* FIX UPS
-
-        https://stackoverflow.com/questions/5405034/attempting-to-create-a-stable-game-engine-loop
-
-        long maxWorkingTimePerFrame = 1000 / 20;  //this is optional
-        unsigned long lastStartTime = millis();
-        while(true)
-        {
-            long elapsedTime = millis() - lastStartTime;
-            lastStartTime = millis();
-
-            // update objects elapsedTime = deltaTime;
-            Serial.println(elapsedTime);
-            ups++;
-            if (lastStartTime > lastTime + 1000)
-            {
-                Serial.print("UPS: ");
-                Serial.println(ups);
-                lastTime = lastStartTime;
-                ups = 0;
-            }
-            ball->move();
-            checkCollisionWithBoard(ball);
-            checkCollisionWithPaddle(ball, paddle1);
-            checkCollisionWithPaddle(ball, paddle2);
-
-
-            //enforcing a maximum framerate here is optional...you don't need to sleep the thread
-            long processingTimeForCurrentFrame = millis() - lastStartTime;
-            if(processingTimeForCurrentFrame < maxWorkingTimePerFrame)
-            {
-                vTaskDelay(pdMS_TO_TICKS(maxWorkingTimePerFrame - processingTimeForCurrentFrame));
-            }
-        }*/
+    // TODO Fix UPS
 
     while (true)
     {
@@ -75,16 +42,15 @@ void GameLoop::update(GameEntity *gameEntities)
         paddle1->moveUsingAI(ball, true);
         paddle2->move();
 
-
         if (paddle1->collideWithBoard(displayProperties))
         {
             paddle1->reverseVelocityY();
-            
+
             /*
             float velocity = random(1, 10) / 10.0f;
             if(paddle1->getVelocityY() < 0){
                 velocity*=-1;
-            }       
+            }
             paddle1->updateVelocity(0, velocity);
             */
         }
@@ -103,9 +69,34 @@ void GameLoop::update(GameEntity *gameEntities)
 
 void GameLoop::checkCollisionWithPaddle(Ball *ball, Paddle *paddle)
 {
+    bool ballIsOnLeft = ball->getPositionX() + ball->getRadius() > paddle->getPositionX();
+    bool ballIsOnRight = ball->getPositionX() - ball->getRadius() < paddle->getPositionX() + paddle->getWidth();
+    bool ballIsOnTop = ball->getPositionY() + ball->getRadius() > paddle->getPositionY();
+    bool ballIsOnBottom = ball->getPositionY() - ball->getRadius() < paddle->getPositionY() + paddle->getHeight();
+
     // Checks for the collisions with the paddle
-    if ((ball->getPositionX() + ball->getRadius() > paddle->getPositionX() && ball->getPositionX() - ball->getRadius() < paddle->getPositionX() + paddle->getWidth()) && ball->getPositionY() > paddle->getPositionY() && ball->getPositionY() < (paddle->getPositionY() + paddle->getHeight()))
+    if (ballIsOnLeft && ballIsOnRight && ballIsOnTop && ballIsOnBottom)
     {
+        float newX = ball->getPositionX();
+        float newY = ball->getPositionY();
+        if (ballIsOnLeft && ball->getVelocityX() > 0)
+        {
+            newX = paddle->getPositionX() - ball->getRadius();
+        }
+        if (ballIsOnRight && ball->getVelocityX() < 0)
+        {
+            newX = paddle->getPositionX() + paddle->getWidth() + ball->getRadius();
+        }
+        if (ballIsOnTop)
+        {
+            newY = paddle->getPositionY() + ball->getRadius();
+        }
+        if (ballIsOnBottom)
+        {
+            newY = paddle->getPositionY() + paddle->getHeight() - ball->getRadius();
+        }
+        ball->setPosition(newX, newY);
+
         ball->reverseVelocityX();
 
         // Add some randomness to the ball motion, based on the current skew setting
@@ -115,3 +106,38 @@ void GameLoop::checkCollisionWithPaddle(Ball *ball, Paddle *paddle)
             ball->reverseVelocityY();
     }
 }
+
+/* FIX UPS
+
+    https://stackoverflow.com/questions/5405034/attempting-to-create-a-stable-game-engine-loop
+
+    long maxWorkingTimePerFrame = 1000 / 20;  //this is optional
+    unsigned long lastStartTime = millis();
+    while(true)
+    {
+        long elapsedTime = millis() - lastStartTime;
+        lastStartTime = millis();
+
+        // update objects elapsedTime = deltaTime;
+        Serial.println(elapsedTime);
+        ups++;
+        if (lastStartTime > lastTime + 1000)
+        {
+            Serial.print("UPS: ");
+            Serial.println(ups);
+            lastTime = lastStartTime;
+            ups = 0;
+        }
+        ball->move();
+        checkCollisionWithBoard(ball);
+        checkCollisionWithPaddle(ball, paddle1);
+        checkCollisionWithPaddle(ball, paddle2);
+
+
+        //enforcing a maximum framerate here is optional...you don't need to sleep the thread
+        long processingTimeForCurrentFrame = millis() - lastStartTime;
+        if(processingTimeForCurrentFrame < maxWorkingTimePerFrame)
+        {
+            vTaskDelay(pdMS_TO_TICKS(maxWorkingTimePerFrame - processingTimeForCurrentFrame));
+        }
+    }*/
--- src/gameEntities/ball.cpp
+++ src/gameEntities/ball.cpp
@@ -8,9 +8,43 @@ Ball::~Ball()
 {
 }
 
-bool Ball::collideWithBoard(DisplayProperties* displayProperties)
+bool Ball::collideWithBoard(DisplayProperties *displayProperties)
 {
     bool collide = false;
+
+    // Check for collisions with the top of the screen
+    if ((positionY - RADIUS - 1) < displayProperties->topLeftY)
+    {
+        positionY = displayProperties->topLeftY + RADIUS + 1;
+        reverseVelocityY();
+        collide = true;
+    }
+
+    // Check for collisions with the bottom of the screen
+    if ((positionY + RADIUS + 1) > displayProperties->bottomLeftY)
+    {
+        positionY = displayProperties->bottomLeftY - RADIUS - 1;
+        reverseVelocityY();
+        collide = true;
+    }
+
+    // Check for collisions with the left of the screen
+    if ((positionX - RADIUS - 1) < displayProperties->topLeftX)
+    {
+        positionX = displayProperties->topLeftX + RADIUS + 1;
+        reverseVelocityX();
+        collide = true;
+    }
+
+        // Check for collisions with the right of the screen
+    if ((positionX + RADIUS + 1) > displayProperties->topRightX)
+    {
+        positionX = displayProperties->topRightX - RADIUS - 1;
+        reverseVelocityX();
+        collide = true;
+    }
+
+    /*
 
     // Check for collisions with the top and bottom of the screen
     if ((positionY - RADIUS - 1) <= displayProperties->topLeftY || (positionY + RADIUS + 1) > displayProperties->bottomLeftY)
@@ -18,13 +52,14 @@ bool Ball::collideWithBoard(DisplayProperties* displayProperties)
         reverseVelocityY();
         collide = true;
     }
-    
+
     // Check for collisions with the left and right of the screen
     if ((positionX - RADIUS - 1) <= displayProperties->topLeftX || (positionX + RADIUS + 1) > displayProperties->topRightX)
     {
         reverseVelocityX();
         collide = true;
     }
+    */
 
     return collide;
 }
